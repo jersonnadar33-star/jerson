798d8190989af57013f42c31c8ff66e63e060e12
jerson <jersonnadar33@gmail.com>
2026-07-16 00:45:24 -0700

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -80,3 +80,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://jerson-production-4d64.up.railway.app/',
+]
